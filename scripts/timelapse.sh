c55e3fbe280a980b31eec014f09a6a5ef4a24398
Craig Burdulis <craigburdulis@CJ-LT.local>
2020-03-15 09:49:37 -0400

    Added date/time annotation

--- scripts/timelapse.sh
+++ scripts/timelapse.sh
@@ -40,7 +40,13 @@ while true; do
                 echo "Created folder: ${FOLDER_NAME}"
         fi
         # Take image
-        raspistill --nopreview -t $CAM_DELAY -o $FOLDER_NAME/$FNAME -w $RES_W -h $RES_H -rot $ROTATE -cfx 128:128
+        raspistill --nopreview \
+                -t $CAM_DELAY \
+                -o $FOLDER_NAME/$FNAME \
+                -w $RES_W -h $RES_H \
+                -rot $ROTATE \
+                -cfx 128:128 \ # Greyscale
+                --annotate 12 --annotateex 64 # Add date/time in 64px
         echo "Captured: ${FNAME}"
 
         # Upload to S3
