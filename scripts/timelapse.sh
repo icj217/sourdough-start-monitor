4e9a0fc5c2c8840cb50c0ff2c14fd4ac0dcef98c
Craig Burdulis <craigburdulis@CJ-LT.local>
2020-03-15 09:42:09 -0400

    Added greyscale to raspistill

--- scripts/timelapse.sh
+++ scripts/timelapse.sh
@@ -40,7 +40,7 @@ while true; do
                 echo "Created folder: ${FOLDER_NAME}"
         fi
         # Take image
-        raspistill --nopreview -t $CAM_DELAY -o $FOLDER_NAME/$FNAME -w $RES_W -h $RES_H -rot $ROTATE
+        raspistill --nopreview -t $CAM_DELAY -o $FOLDER_NAME/$FNAME -w $RES_W -h $RES_H -rot $ROTATE -cfx 128:128
         echo "Captured: ${FNAME}"
 
         # Upload to S3
